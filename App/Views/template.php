ab899ccbfc7636af63a9cdc5800488eb1a4397ce
Khusam Alfas <halfas@bk.ru>
2023-10-19 11:25:21 +0300

    Docker & create public_html folder

--- App/Views/template.php
+++ App/Views/template.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-     <link rel="stylesheet" type="text/css" href="/resources/css/styles.css" />
+     <link rel="stylesheet" type="text/css" href="/public_html/resources/css/styles.css" />
 
     <title>YADIRECTAPP</title>
 </head>
@@ -21,9 +21,9 @@
 
 
 <footer>
-    <script type="text/javascript" src="/resources/javascript/js-loader.js"></script>
-    <script type="text/javascript" src="/resources/javascript/js.js"></script>
-    <script type="text/javascript" src="/resources/javascript/js-sort.js"></script>
-    <script type="text/javascript" src="/resources/javascript/js-paggination.js"></script>
+    <script type="text/javascript" src="/public_html/resources/javascript/js-loader.js"></script>
+    <script type="text/javascript" src="/public_html/resources/javascript/js.js"></script>
+    <script type="text/javascript" src="/public_html/resources/javascript/js-sort.js"></script>
+    <script type="text/javascript" src="/public_html/resources/javascript/js-paggination.js"></script>
 </footer>
 </html>
